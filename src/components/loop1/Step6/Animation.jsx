825f2c3a2e64ac361c7837ba0aa35d4c6e919771
incredible92 <sulymanabdullahi@gmail.com>
2021-03-16 12:54:30 +0000

    update:loop1_step6

--- src/components/loop1/Step6/Animation.jsx
+++ src/components/loop1/Step6/Animation.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
 
 import './Animation.scss'
 
  function Animation() {
+     const[horizontal, setHorizontal] = useState(0);
+     const[vertical, setVertical] = useState(0)
+    
+
+     useEffect(() => {
+        const event = document.querySelector("body");
+        event.addEventListener("mousemove", (e) => {
+          setVertical(e.clientX / 90);
+          setHorizontal(e.clientY / 90);
+        });
+
+        return () => {
+            event.removeEventListener("mousemove", () => { });
+          };
+        }, []);
+
+    //      const setEvent = e => setDirection({horizonatal :e.clientX, vertical:e.clientY})
+    //      window.addEventListener('mousemove', setEvent)
+    //      return () => {
+    //          window.removeEventListener('mousemove', setEvent)
+    //      }
+    //  }, [direction])
+    
     return (
+
         <div className="bg6">
             <div className="aniwrap">
-                <span className="main-content"></span>
+                <span className="main-content"  style={{
+          transform: `rotateX(${horizontal}deg) rotateY(${vertical}deg)`
+        }}></span>
             </div>
         </div>
     )
