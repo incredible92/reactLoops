0b797326fa37139be3dd742cf2e27b0591ccbdd6
incredible92 <sulymanabdullahi@gmail.com>
2021-03-17 10:40:26 +0000

    update:loop1_step7

--- src/components/loop1/Step7/Scroll.jsx
+++ src/components/loop1/Step7/Scroll.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React,{useRef, useEffect} from "react";
 
 import "./Scroll.scss";
 
+
+
 function Scroll() {
+  const myRef = useRef(null)
+  useEffect(() => {
+    window.scrollTo({
+      behavior: "smooth",
+      top: myRef.current.offsetTop,
+    });
+   
+  }, [myRef.current])
   return (
     <div className="bg7">
-      <div className="scrollWrap">
-        <div className="scrollElement"></div>
-        <div className="scrollElement"></div>
+      <div className="scrollWrap" ref={myRef}>
         <div className="scrollElement"></div>
       </div>
     </div>
