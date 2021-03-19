e9303d40752928a2aa922523dfa392088f660a3c
incredible92 <sulymanabdullahi@gmail.com>
2021-03-19 10:56:25 +0000

    feat: loop1_step10

--- src/components/loop1/Step10/Drag.jsx
+++ src/components/loop1/Step10/Drag.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
+import Draggable from "react-draggable";
 
-import './Drag.scss'
+import "./Drag.scss";
 
 function Drag() {
-    return (
-        <div className="bg10">
-            <div className="dragWrap">
-                <div className="dragElement"></div>
-            </div>
-        </div>
-    )
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const dragEvent = (action) => {
+    setPosition({ x: action.x, y: action.y });
+  };
+
+  return (
+    <div className="bg10">
+      <div className="dragWrap">
+        <Draggable onDrag={(e, action) => dragEvent(action)}>
+          <div className="dragElement"></div>
+        </Draggable>
+      </div>
+    </div>
+  );
 }
 
-export default  Drag
+export default Drag;
