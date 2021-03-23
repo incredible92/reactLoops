73bd1d391398fa545685a9bf092cda6c24429175
incredible92 <sulymanabdullahi@gmail.com>
2021-03-23 10:53:30 +0000

    fix:Step4

--- src/components/loop1/Step4/Data.jsx
+++ src/components/loop1/Step4/Data.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./Data.scss";
 
 function Data() {
+  const methods = "Choose title, Choose description, Confirm data".split(", ");
+  const [nextIndex, setNextIndex] = useState(0);
+
+  const nextStep = (index) => {
+    if (index === 2 && nextIndex === 0) return;
+    setNextIndex(index);
+  }
   return (
     <div className="bg4">
       <div className="dataWrap">
